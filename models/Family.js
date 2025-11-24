54129cb5a8f5a1a62195981ea856aa27ab9f9615
Michael Zarif <michael.mirsal@gmail.com>
2025-11-24 18:05:40 +0200

    Family.js to MongoDB

--- models/Family.js
+++ models/Family.js
@@ -3,20 +3,21 @@ import mongoose from "mongoose";
 
 const memberSchema = new mongoose.Schema(
   {
-    id: { type: Number, required: true },
+    // id: { type: Number, required: true },  <-- remove
     name: { type: String, required: true },
     role: { type: String, default: "kid" }
   },
-  { _id: false } // <--- This disables MongoDB auto _id for members
+  { _id: false }
 );
 
 const familySchema = new mongoose.Schema({
-  id: { type: Number, required: true }, // numeric ID like original
+  // id: { type: Number, required: true }, <-- remove
   name: { type: String, required: true, unique: true },
   dad: { type: String, required: true },
   mom: { type: String, required: true },
   passhash: { type: String, required: true },
   members: { type: [memberSchema], default: [] }
 });
+
 
 export default mongoose.model("Family", familySchema);
