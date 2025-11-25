ebfeb546ef0f4b255c6fd21bf8aa5c68281282f6
Michael Zarif <michael.mirsal@gmail.com>
2025-11-25 19:23:11 +0200

    Family.js to MongoDB

--- models/Family.js
+++ models/Family.js
@@ -1,15 +1,6 @@
 // models/Family.js
 import mongoose from "mongoose";
 
-const memberSchema = new mongoose.Schema(
-  {
-    // id: { type: Number, required: true },  <-- remove
-    name: { type: String, required: true },
-    role: { type: String, default: "kid" }
-  },
-  { _id: false }
-);
-
 
 const familySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
