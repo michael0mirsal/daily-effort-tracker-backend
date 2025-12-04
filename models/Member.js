a9d2d750c3ce06a525d9c976085b7a2ffd6e7e3e
Michael Zarif <michael.mirsal@gmail.com>
2025-12-04 14:51:26 +0200

    Members.js

--- models/Member.js
+++ models/Member.js
@@ -4,7 +4,8 @@ const memberSchema = new mongoose.Schema({
   name: { type: String, required: true },
   role: { type: String, default: "kid" },
   age: { type: Number, default: null },        // optional
-  family: { type: mongoose.Schema.Types.ObjectId, ref: "Family", required: true }
+  family: { type: mongoose.Schema.Types.ObjectId, ref: "Family", required: true },
+  avatar: { type: String, default: '' }, // <-- new field
 }, { timestamps: true });
 
 export default mongoose.model("Member", memberSchema);
