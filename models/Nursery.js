fee58c056b3e56a9eefcaf96076c8ce116d30ebf
Michael Zarif <michael.mirsal@gmail.com>
2025-12-05 23:03:10 +0200

    Nursery.js

--- models/Nursery.js
+++ models/Nursery.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
 const NurserySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  address: { type: String },     // <--- ADD THIS
+  name: { type: String, required: true, unique: true }, // Nursery Name
+  address: { type: String },                              // Optional
+  contactEmail: { type: String, required: true },         // Contact Email
+  contactPhone: { type: String, required: true },         // Contact Phone
+  passKey: { type: String, required: true },             // Nursery Pass Key
   classes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Class" }]
-});
+}, { timestamps: true }); // adds createdAt and updatedAt
 
 export default mongoose.model("Nursery", NurserySchema);
