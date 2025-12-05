37cfa30eb4e79f587076c885490680944d379b5d
Michael Zarif <michael.mirsal@gmail.com>
2025-12-05 13:34:11 +0200

    Teacher.js, Class.js

--- models/Class.js
+++ models/Class.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const classSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  nursery: { type: mongoose.Schema.Types.ObjectId, ref: "Nursery", required: true },
-  teachers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Teacher" }],
-  kids: [{ type: mongoose.Schema.Types.ObjectId, ref: "Member" }] // link to existing kids
+  nursery: { type: mongoose.Schema.Types.ObjectId, ref: "Nursery" },
+  teacher: { type: mongoose.Schema.Types.ObjectId, ref: "Teacher" },
+  kids: [{ type: mongoose.Schema.Types.ObjectId, ref: "Member" }]
 });
 
 export default mongoose.model("Class", classSchema);
