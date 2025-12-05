3faf164de4b8a29376871422e64a54c55005995b
Michael Zarif <michael.mirsal@gmail.com>
2025-12-05 13:28:09 +0200

    Teacher.js

--- models/Teacher.js
+++ models/Teacher.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const teacherSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
+  phone: { type: String },
+  nursery: { type: mongoose.Schema.Types.ObjectId, ref: "Nursery" },
   classes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Class" }]
 });
 
