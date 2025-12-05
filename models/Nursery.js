5426bb82a3c8d40c908b7fe869744b15fc1cc522
Michael Zarif <michael.mirsal@gmail.com>
2025-12-05 13:16:07 +0200

    Nersury.js

--- models/Nursery.js
+++ models/Nursery.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
-const nurserySchema = new mongoose.Schema({
+const NurserySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
+  address: { type: String },     // <--- ADD THIS
   classes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Class" }]
 });
 
-export default mongoose.model("Nursery", nurserySchema);
+export default mongoose.model("Nursery", NurserySchema);
