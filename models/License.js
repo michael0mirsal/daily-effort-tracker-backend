2f5baa1a418fcccb685b3567e9b8a0d23cb6cab4
Michael Zarif <michael.mirsal@gmail.com>
2025-12-06 23:04:24 +0200

    License.js

--- models/License.js
+++ models/License.js
@@ -4,6 +4,7 @@ const licenseSchema = new mongoose.Schema({
   key: { type: String, required: true, unique: true },
   used: { type: Boolean, default: false },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'Nursery', default: null },
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model('License', licenseSchema);
