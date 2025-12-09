4078e203bbaee123e3922cecb1e7ed770c05bfd8
Michael Zarif <michael.mirsal@gmail.com>
2025-12-09 21:09:37 +0200

    schoolRoutes.js

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -187,7 +187,7 @@ router.post("/school/signin", async (req, res) => {
       }
 
       user = {
-        _id: Teacher._id,
+        _id: user._id,
         name: user.name,
         role,
         nurseryId: user.nursery._id,
