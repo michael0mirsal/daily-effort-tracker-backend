994a4e0ed82e08fb10dc87931ae76f6b77e2438e
Michael Zarif <michael.mirsal@gmail.com>
2025-12-16 14:02:16 +0200

    sch-activity.js

---
+++ models/sch-activity.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const activitySchema = new mongoose.Schema({
+classId: { type: mongoose.Schema.Types.ObjectId, ref: "Class", required: true },
+kidmember: { type: mongoose.Schema.Types.ObjectId, ref: "SchoolMember", required: true },
+  date: { type: String, required: true },
+  items: [
+    {
+      activity: String,
+      timeMin: Number,
+      evaluation: Number,
+      note: String
+    },
+  ],
+  checkedData: Number,
+});
+
+export default mongoose.model("sch-activity", activitySchema);
