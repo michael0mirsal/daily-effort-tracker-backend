fdde533772b69e36ae2fd7b53043cfbb1c158e97
Michael Zarif <michael.mirsal@gmail.com>
2025-12-21 19:11:35 +0200

    server.js

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const PORT = process.env.PORT || 3000; // Railway prefers 8080
 // ======================================================
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public/school-front")));
 app.use("/api", schoolRoutes);
 app.use("/api", schTaskRoutes);
