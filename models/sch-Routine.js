fda2fc71f58b7f899c601ce910ca18a2bce06184
Michael Zarif <michael.mirsal@gmail.com>
2025-12-24 00:05:59 +0200

    sch-Routine.js

--- models/sch-Routine.js
+++ models/sch-Routine.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const routineSchema = new mongoose.Schema({
   classId: { type: mongoose.Schema.Types.ObjectId, ref: "Class", required: true },
-  kidmember: { type: mongoose.Schema.Types.ObjectId, ref: "SchoolMember", required: true },
-
+schoolMember: { type: mongoose.Schema.Types.ObjectId, ref: "SchoolMember", required: true },
   date: { type: String, required: true }, // "2025-01-03"
 
   items: [
