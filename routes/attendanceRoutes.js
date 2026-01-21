5836a7c7aa9fa2f40df3283c760f06de7ed805df
Michael Zarif <michael.mirsal@gmail.com>
2026-01-21 17:16:46 +0200

    attendanceRoutes.js

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -8,6 +8,15 @@ import {
 
 const router = express.Router();
 
+/* TEST ROUTE */
+router.post("/attendance", async (req, res) => {
+  console.log("🔥 ATTENDANCE ROUTE HIT");
+  console.log("BODY:", req.body);
+
+  res.json({ ok: true });
+});
+
+/* REAL ROUTES */
 router.post("/", createAttendance);
 router.get("/", getAttendanceByClass); // query params: classId, date
 router.put("/:id", updateAttendance);
