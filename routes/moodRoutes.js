77e7be95f8509479d94d927001c8e8a8723f0c60
Michael Zarif <michael.mirsal@gmail.com>
2026-02-06 15:32:10 +0200

    moodRoutes.js

--- routes/moodRoutes.js
+++ routes/moodRoutes.js
@@ -17,5 +17,5 @@ router.post("/", saveKidMood);
  * GET /api/mood?classId=xxx&date=yyyy-mm-dd
  */
 router.get("/", getMoodsByClassAndDate);
-
+router.get("/mood/kid", getMoodByKidAndDate); // ✅ new route
 export default router;
