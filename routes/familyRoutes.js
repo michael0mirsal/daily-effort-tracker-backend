d40683c387be4d3ecc48e077e43ac33c1e4cfb2b
Michael Zarif <michael.mirsal@gmail.com>
2026-02-10 15:26:54 +0200

    familyRoutes.js

--- routes/familyRoutes.js
+++ routes/familyRoutes.js
@@ -85,10 +85,7 @@ router.post("/signup", async (req, res) => {
 // ✅ POST /signin
 // ======================================================
 // ======================================================
-app.get("/testcookie", (req, res) => {
-  console.log("Cookies received:", req.cookies);
-  res.json({ cookies: req.cookies });
-});
+
 // ✅ POST /signin
 router.post("/signin", async (req, res) => {
   const { family, name, passkey } = req.body;
