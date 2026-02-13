b452b762d4af2277485c87fbac49f95b1d6b90fa
Michael Zarif <michael.mirsal@gmail.com>
2026-02-13 15:39:56 +0200

    PushSubscription.js

---
+++ models/PushSubscription.js
@@ -0,0 +1,13 @@
+// src/models/PushSubscription.js
+import mongoose from "mongoose";
+
+const PushSubscriptionSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // optional
+  endpoint: { type: String, required: true },
+  keys: {
+    p256dh: { type: String, required: true },
+    auth: { type: String, required: true },
+  },
+}, { timestamps: true });
+
+export default mongoose.model("PushSubscription", PushSubscriptionSchema);
